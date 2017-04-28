6a5b9a051bafbea9844745eeef5779a80f651457
iaktas <ilkayaktas@gmail.com>
2017-04-28 18:31:46 +0300

    Main screen is redesigned. Stat view is removed. Date picker is added.

--- app/src/main/java/edu/metu/sucre/views/activities/home/MainActivity.java
+++ app/src/main/java/edu/metu/sucre/views/activities/home/MainActivity.java
@@ -4,36 +4,35 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import com.github.jhonnyx2012.horizontalpicker.DatePickerListener;
+import com.github.jhonnyx2012.horizontalpicker.HorizontalPicker;
+
+import org.joda.time.DateTime;
+
 import java.util.List;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import edu.metu.sucre.R;
-import edu.metu.sucre.adapters.ListAdapter;
 import edu.metu.sucre.model.app.ListItem;
 import edu.metu.sucre.views.activities.base.BaseActivity;
-import edu.metu.sucre.views.activities.sugarlevel.SugarLevelActivity;
 import edu.metu.sucre.views.widgets.dialogs.rateme.Config;
 import edu.metu.sucre.views.widgets.dialogs.rateme.RateMe;
 
-public class MainActivity extends BaseActivity implements MainMvpView {
+public class MainActivity extends BaseActivity implements MainMvpView, DatePickerListener {
     
     @Inject
     MainMvpPresenter<MainMvpView> mPresenter;
 
     @BindView(R.id.welcome) TextView welcome;
-    @BindView(R.id.resultView) TextView resultView;
-    @BindView(R.id.list_of_sugarlevel) ListView listOfSugarLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +44,10 @@ public class MainActivity extends BaseActivity implements MainMvpView {
         setUnBinder(ButterKnife.bind(this));
     
         RateMe.init(new Config(5, 10)); // 5 gün ya da 10 defa uygulama başlattıktan sonra
-    
+
+        HorizontalPicker picker= (HorizontalPicker) findViewById(R.id.datePicker);
+        picker.setListener(this).init();
+        picker.setDate(DateTime.now());
         // Attach presenter
         mPresenter.onAttach(MainActivity.this);
     
@@ -53,18 +55,8 @@ public class MainActivity extends BaseActivity implements MainMvpView {
     
         setFonts();
 
-        temporaryInitializeList();
     }
 
-    private void temporaryInitializeList() {
-        List<ListItem> list = new ArrayList<>();
-        for (int i = 0; i < 15; i++) {
-            ListItem listItem = new ListItem(120+5*i, i%2==0?"pre":"post", "24.04.2017");
-            list.add(listItem);
-        }
-
-        updateListView(list);
-    }
 
     @Override
     protected void onStart() {
@@ -116,17 +108,14 @@ public class MainActivity extends BaseActivity implements MainMvpView {
     
     private void setFonts(){
         welcome.setTypeface(typeface);
-        resultView.setTypeface(typeface);
     }
 
     @Override
     public void updateListView(List<ListItem> sugarValues) {
-        ListAdapter adapter = new ListAdapter(this, sugarValues);
-        listOfSugarLevel.setAdapter(adapter);
     }
-    
-    @OnClick(R.id.resultView)
-    public void showListView(View view) {
-        startActivity(SugarLevelActivity.class);
+
+    @Override
+    public void onDateSelected(DateTime dateSelected) {
+        Log.i("HorizontalPicker","Fecha seleccionada="+dateSelected.toString());
     }
 }
